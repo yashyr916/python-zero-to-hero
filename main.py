6be993a9820c6972fb471d76277bb699ad4c7079
Yash Raj Chandra <yashyr916@gmail.com>
2026-01-02 20:05:31 +0530

    Update: added index access example

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ a = 5
 b = 6
 c = a + b
 print(c)
+
+
+# Program Number 2
+length = int(input("Enter The Length Of Rectangle: "))
+breadth = int(input("Enter The Breadth Of Rectangle: "))
+area = length * breadth
+print("area is ",area)
